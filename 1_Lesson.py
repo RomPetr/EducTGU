fd5eb06d6119c94427056c07185b63bf6e7c7f64
Rompetr78 <rompetr@yahoo.com>
2024-10-01 21:26:51 +0800

    first lesson

---
+++ 1_Lesson.py
@@ -0,0 +1,47 @@
+
+"""
+first = 5
+second = 6
+rez = first + second
+print(first, "+", second, "=", rez)
+
+first_txt = "Hello"
+second_txt = " Python!"
+rez = first_txt + second_txt
+print(rez)
+
+first = "Hello "
+second = 100
+rez = first * second
+print(rez)
+
+first = "100"
+second = "500"
+rez = first + second
+print(rez)
+
+first = 4
+second = "5"
+rez = first + int(second)
+print(rez)
+
+first = 4
+second = "5"
+rez = second + str(first)
+print(rez)
+
+first = input()
+second = 5
+rez = second + int(first)
+print(rez)
+
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+rez = first * second
+print("Результат умножения = ", rez)
+
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+rez = first + second
+print(str(first) + "+" + str(second) + "=" + str(rez))
+"""
