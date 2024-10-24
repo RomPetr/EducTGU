91954b2817b028cc99bad4618ed3bfeae238dd33
Rompetr78 <rompetr@yahoo.com>
2024-10-24 23:18:54 +0800

    added some chenges

--- 11_3_Lesson.py
+++ 11_3_Lesson.py
@@ -3,10 +3,10 @@ from tkinter import *
 
 window = Tk()
 window.title("Главное окно")
-w = window.winfo_screenmmwidth()
+w = window.winfo_screenwidth()
 h = window.winfo_screenheight()
 print(f"Размер вашего экрана {w} на {h} пикселей")
-w1 = w // 2
+w1 = w // 2 - 200
 h1 = h // 2 - 150
 window.geometry(f"400x300+{w1}+{h1}")
 window.mainloop()
--- 11_5_1_Lecture_files.py
+++ 11_5_1_Lecture_files.py
@@ -1,3 +1,4 @@
+"""
 from tkinter import *
 from tkinter import filedialog as fd
 
@@ -28,3 +29,57 @@ btn2 = Button(window, text="Сохранить текст", command=text_save)
 btn2.pack()
 
 window.mainloop()
+"""
+
+"""
+# радиокнопки
+import tkinter as tk
+
+root = tk.Tk()
+
+# Создаем переменную для хранения значения выбранной радиокнопки
+selected_option = tk.StringVar(value="Option1")
+
+# Создаем радиокнопки и связываем их с переменной selected_option
+radio1 = tk.Radiobutton(root, text="Option 1", variable=selected_option, value="Option1")
+radio2 = tk.Radiobutton(root, text="Option 2", variable=selected_option, value="Option2")
+radio3 = tk.Radiobutton(root, text="Option 3", variable=selected_option, value="Option3")
+
+radio1.pack()
+radio2.pack()
+radio3.pack()
+
+# Функция для получения текущего значения переменной
+def show_selected():
+    print("Текущая выбранная опция:", selected_option.get())
+
+# Кнопка для вызова функции show_selected
+button = tk.Button(root, text="Показать выбранную опцию", command=show_selected)
+button.pack()
+
+root.mainloop()
+"""
+
+"""
+# изменения свойств кнопки
+import tkinter as tk
+
+root = tk.Tk()
+
+# Создаем кнопку с начальными параметрами
+button = tk.Button(root, text="Нажми меня", bg="blue", fg="white")
+button.pack(pady=20)
+
+# Функция для изменения свойств кнопки
+def change_button_properties():
+    button.config(text="Изменено!", bg="green", fg="black", font=("Arial", 14))
+
+# Кнопка для изменения свойств первой кнопки
+change_button = tk.Button(root, text="Изменить свойства", command=change_button_properties)
+change_button.pack(pady=20)
+
+root.mainloop()
+"""
+
+greeting = "Hello, world!"
+print(greeting[:5])
