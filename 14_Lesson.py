3904a4b5d6033489a909d4bb02fac8f19b3ce4cb
Rompetr78 <rompetr@yahoo.com>
2024-10-31 19:08:58 +0800

    В ф-ции 'set_reminder' добавлена пер-я r_time в кот. хранится время в кот. должно сработать напоминание в файле 14_Lesson.py

--- 14_Lesson.py
+++ 14_Lesson.py
@@ -2,14 +2,22 @@ from tkinter import *
 import time
 import datetime
 from tkinter import simpledialog as sd
+from tkinter import messagebox as mb
 
 def set_reminder():
     time_reminder = sd.askstring(title="Время напоминания", prompt="Введите время в формате ЧЧ:ММ")
     if time_reminder:
-        hour, minute = time_reminder.split(":")
-        hour = int(hour)
-        minute = int(minute)
-        now_time = datetime.datetime.now()
+        try:
+            hour, minute = time_reminder.split(":")
+            hour = int(hour)
+            minute = int(minute)
+            now_time = datetime.datetime.now()
+            print(now_time)
+            time_r = now_time.replace(hour=hour, minute=minute)
+            print(time_r)
+            mb.showinfo(title="Успех", message=f"Напоминание установлено на {hour}:{minute}")
+        except ValueError:
+            mb.showerror("Ошибка", f"Неправильно указано время")
 
 
 window = Tk()
