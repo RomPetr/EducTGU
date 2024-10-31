cf3dce7201432f2e134efbe5986d3335aba04ccf
Rompetr78 <rompetr@yahoo.com>
2024-10-31 18:48:57 +0800

    Создан файл с граф. интерф. и ф-ция (set_reminder) для получения времени от поль-ля в 14_Lesson.py

---
+++ 14_Lesson.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import time
+import datetime
+from tkinter import simpledialog as sd
+
+def set_reminder():
+    time_reminder = sd.askstring(title="Время напоминания", prompt="Введите время в формате ЧЧ:ММ")
+    if time_reminder:
+        hour, minute = time_reminder.split(":")
+        hour = int(hour)
+        minute = int(minute)
+
+
+window = Tk()
+window.title("Напоминалка")
+width_d = window.winfo_screenmmwidth()
+height_d = window.winfo_screenheight()
+print(width_d, height_d)
+window.geometry(f"300x200+{width_d//2-150}+{height_d//2-100}")
+
+l = Label(window, text="--Нажмите на кнопку и установите\n напоминание на любое время--")
+l.pack(pady=5)
+
+btn = Button(window, text="Установить напоминание", command=set_reminder)
+btn.pack(pady=5)
+
+
+window.mainloop()
