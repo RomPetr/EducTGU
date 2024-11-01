73b37800ded8036d0cb129333d3f96c92339c838
Rompetr78 <rompetr@yahoo.com>
2024-11-01 14:20:50 +0800

    добавлена функция остановки музыки 'stop_music' и кнопка 'btn_stop_music' в файл 14_Lesson.py

--- 14_Lesson.py
+++ 14_Lesson.py
@@ -18,6 +18,7 @@ def set_reminder():
             print(now_time)
             r_time = now_time.replace(hour=hour, minute=minute, second=0)
             print(r_time)
+            r_time = r_time.timestamp()
             mb.showinfo(title="Успех", message=f"Напоминание установлено на {hour}:{minute}")
             check_time()
         except ValueError:
@@ -29,8 +30,8 @@ def check_time():
     if r_time:
         now = time.time()
         print(f"Текущее время - {now}")
-        print(f"Напоминание - {r_time.timestamp()}")
-        if now >= r_time.timestamp():
+        print(f"Напоминание - {r_time}")
+        if now >= r_time:
             # print("Играет музыка")
             play_music()
             r_time = None
@@ -39,12 +40,22 @@ def check_time():
 
 
 def play_music():
+    global music
     pygame.mixer.init()
     pygame.mixer.music.load("reminder.mp3")
     pygame.mixer.music.play()
+    music = True
 
 
-r_time = None
+def stop_music():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music = False
+
+
+music = False
+r_time = 0
 
 window = Tk()
 window.title("Напоминалка")
@@ -59,5 +70,7 @@ l.pack(pady=5)
 btn = Button(window, text="Установить напоминание", command=set_reminder)
 btn.pack(pady=5)
 
+btn_stop_music = Button(window, text="Остановить музыку", command=stop_music)
+btn_stop_music.pack(pady=5)
 
 window.mainloop()
