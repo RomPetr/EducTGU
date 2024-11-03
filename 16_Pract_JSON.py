9bac6a2c80ac583701f32ef142c3b75d539c20ee
Rompetr78 <rompetr@yahoo.com>
2024-11-03 23:48:44 +0800

    Создана ветка "Combobox". В файле 16_Pract_JSON.py добавлен модуль ttk и реализован combobox

--- 16_Pract_JSON.py
+++ 16_Pract_JSON.py
@@ -4,6 +4,7 @@ import json
 import pprint
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import ttk
 
 """
 result = requests.get('https://open.er-api.com/v6/latest/USD')
@@ -14,12 +15,12 @@ p.pprint(data)
 
 
 def exchange():
-    code = entry.get()
+    code = combobox.get()
 
     if code:
         try:
             response = requests.get('https://open.er-api.com/v6/latest/USD')
-            response.raise_for_status()
+            response.raise_for_status() # Проверяем, не произошла ли ошибка HTTP
             data = response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
@@ -32,14 +33,32 @@ def exchange():
         mb.showwarning("Внимание!", "Введите код валюты.")
 
 
+cur = {
+    'RUB': 'Российский рубль',
+    'EUR': 'Евро',
+    'GBP': 'Британский фунт стерлингов',
+    'JPY': 'Японская йена',
+    'CNY': 'Китайский юань',
+    'KZT': 'Казахский тенге',
+    'UZS': 'Узбекский сум',
+    'AED': 'Дирхам ОАЭ',
+    'CAD': 'Канадский доллар',
+    'USD': 'Американский доллар'
+}
+
 window = Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
 
-Label(text="Введите код валюты").pack(padx=10, pady=10)
+Label(text="Выберите код валюты").pack(padx=10, pady=10)
 
-entry = Entry()
-entry.pack(padx=10, pady=10)
+# Список 10 популярных валют
+popular_currencies = ["EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "RUB", "KZT", "UZS"]
+combobox = ttk.Combobox(values=popular_currencies)
+combobox.pack(padx=10, pady=10)
+
+# entry = Entry()
+# entry.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
 
