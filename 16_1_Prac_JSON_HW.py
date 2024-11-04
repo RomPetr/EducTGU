4d0a86b0caaa664e03666c977e22e0cb72112e67
Rompetr78 <rompetr@yahoo.com>
2024-11-04 23:22:39 +0800

    создано главное окно программы в 16_1_Pract_JSON_HW.py

--- 16_1_Prac_JSON_HW.py
+++ 16_1_Prac_JSON_HW.py
@@ -22,3 +22,19 @@ p = pprint.PrettyPrinter(indent=3)
 p.pprint(data)
 # -------------------------------------
 """
+
+
+def exchange()
+
+window = Tk()
+window.title("Курсы обмена валют")
+window.geometry("360x180")
+
+Label(text="Введите код валюты").pack(padx=10, pady=10)
+
+entry = Entry()
+entry.pack(padx=10, pady=10)
+
+Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
+
+window.mainloop()
