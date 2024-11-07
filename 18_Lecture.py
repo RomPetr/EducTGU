7fbe1ae2c772d83933d01bcb24c0f338fc2f7fac
Rompetr78 <rompetr@yahoo.com>
2024-11-07 14:32:46 +0800

    в 18_Lesson.py реализована выгрузка файлов на сервис file.io

--- 18_Lecture.py
+++ 18_Lecture.py
@@ -1,11 +1,33 @@
 from tkinter import *
 import requests as r
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
+
+
+def load_file():
+    try:
+        file = fd.askopenfilename()
+        if file:
+            answer = r.post("https://file.io", files={"file": open(file, 'rb')})
+            if answer.status_code == 200:
+                link_file = answer.json()["link"]
+                e.insert(0, link_file)
+            else:
+                print("Не удалось связаться с file.io")
+    except Exception as exc:
+        mb.showerror("Ошибка", f"Ошибка: {exc}")
+
 
 
 window = Tk()
 window.title("Обмен файлами")
 window.geometry("300x150")
 
+
+btn = Button(window, text="Загрузить файл", command=load_file)
+btn.pack(pady=10)
+
+e = Entry(window, font=('Arial', 12))
+e.pack(pady=10)
 
 window.mainloop()
