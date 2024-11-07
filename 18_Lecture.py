aa4dddb9b5ace86defa7a946b0a76c1fe50a0169
Rompetr78 <rompetr@yahoo.com>
2024-11-07 15:22:28 +0800

    Создана функция 'save_files' для сохранения истории загрузок файлов. Необходимо добавить обработку русских букв

--- 18_Lecture.py
+++ 18_Lecture.py
@@ -3,6 +3,20 @@ import requests as r
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 import pyperclip
+import os
+import json
+
+def save_files(path_file, link_file, name_file):
+    history = []
+    if os.path.exists("History.json"):
+        with open("History.json", "r", encoding="utf-8") as f:
+            history = json.load(f)
+
+    history.append({"path_file": path_file, "name_file": name_file, "link_file": link_file})
+
+    with open("History.json", "w", encoding="utf-8") as f:
+        json.dump(history, f, indent=4)
+
 
 
 def load_file():
@@ -12,9 +26,11 @@ def load_file():
             answer = r.post("https://file.io", files={"file": open(file, 'rb')})
             if answer.status_code == 200:
                 link_file = answer.json()["link"]
+                name_file = answer.json()["name"]
                 e.delete(0, END)
                 e.insert(0, link_file)
                 pyperclip.copy(link_file)
+                save_files(file, link_file, name_file)
             else:
                 print("Не удалось связаться с file.io")
     except Exception as exc:
